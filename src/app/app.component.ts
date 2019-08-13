68a7b5393a9ac603c6790c53c8b90d55130a8c71
Anand C <anandchakru.forum@gmail.com>
2019-08-12 23:42:55 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ClogService } from '@nivite/nlib';
 
 @Component({
   selector: 'nivite-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'n00001';
+  fireconf: any; // provide the firebase config here or in index.html
+  guest: any;
+  invite: any;
+  constructor(private clog: ClogService) { }
+  loadInviteData(invite: any) {
+    this.invite = invite;
+    this.clog.log('invite: ' + (invite ? invite.hostName : invite));
+  }
+  loadUserData(user: any) {
+    this.clog.log('user: ' + (user ? user.email : user));
+  }
+  loadGuestData(guest: any) {
+    this.guest = guest;
+    this.clog.log('guest: ' + (guest ? guest.email : guest));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NlibModule } from '@nivite/nlib';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NlibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
